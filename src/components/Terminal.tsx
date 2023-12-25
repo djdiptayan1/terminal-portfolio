bfa68e15434d7a9a80bb6672e179666f7ed5c141
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-25 22:13:47 +0530

    Update secret command tab size

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -31,7 +31,7 @@ export const commands: Command = [
   { cmd: "clear", desc: "clear the terminal", tab: 8 },
   { cmd: "echo", desc: "print out anything", tab: 9 },
   // { cmd: "education", desc: "my education background", tab: 4 },
-  { cmd: "secret", desc: "secret command", tab: 10 },
+  { cmd: "secret", desc: "secret command", tab: 7 },
   { cmd: "email", desc: "send an email to me", tab: 8 },
   { cmd: "gui", desc: "go to my portfolio in GUI", tab: 10 },
   { cmd: "help", desc: "check available commands", tab: 9 },
