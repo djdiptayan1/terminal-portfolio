adc56ae7a0c65c2fc414bb2cd7d9de5bbaf24636
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-26 11:46:37 +0530

    Add tailwind.config.js file with custom green color

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'custom-green': 'rgb(154,207,69)',
+      }
+    }
+  },
+  plugins: [],
+}
+
